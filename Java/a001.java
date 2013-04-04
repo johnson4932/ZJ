91b5a331838f7ebfb8094b4650299e6f62fd3e85
Li-Yu Lu <johnson4932@gmail.com>
2013-04-05 00:38:53 +0800

    add a001.java

---
+++ Java/a001.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class a001 {
+	public static void main(String [] argv){
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			String text = in.next();
+			System.out.println("hello, " + text);
+		}
+	}
+}
