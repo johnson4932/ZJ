d891c3b9e25abcb2f6e0d542525815b6b5d34891
Li-Yu Lu <johnson4932@gmail.com>
2013-04-05 17:55:57 +0800

    add d063.java

---
+++ Java/d063.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class d063 {
+	public static void main(String [] argv) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			int n = in.nextInt();
+			System.out.println((n == 1) ? (0) : (1));
+		}
+	}
+}
