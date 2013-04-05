ca9871627a0afe64be164950cb42ea46404a1c1f
Li-Yu Lu <johnson4932@gmail.com>
2013-04-05 20:26:28 +0800

    add a015.java

---
+++ Java/a015.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class a015 {
+	public static void main(String [] argv) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			int n = in.nextInt();
+			int m = in.nextInt();
+			int [][] arr = new int[n][m];
+			
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					arr[i][j] = in.nextInt();
+				}
+			}
+			
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					System.out.print(arr[j][i] + " ");
+				}
+				System.out.println();
+			}
+		}
+	}
+}
