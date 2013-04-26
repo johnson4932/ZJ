0487a5548d3475252b3f52a5cd07b16dea57118b
Li-Yu Lu <johnson4932@gmail.com>
2013-04-26 20:48:05 +0800

    add d049.java

---
+++ Java/d049.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class d049 {
+	public static void main(String [] argv) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			int y = in.nextInt();
+			System.out.println(y - 1911);
+		}
+	}
+}
