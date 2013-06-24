02d7f000df7c230357544596f4f90f1558678f39
JohnsonLu <johnson4932@gmail.com>
2013-06-24 17:06:49 +0800

    add d126.java

---
+++ Java/d126.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class d126 {
+    public static void main(String [] argv) {
+        Scanner cin = new Scanner(System.in);
+        while (cin.hasNext()) {
+            int a = cin.nextInt();
+            int b = cin.nextInt();
+            System.out.println(a*2 + b*2);
+        }
+    }
+}
