892a4f3b2bb8b39506a06e859b784b6714d33c8d
JohnsonLu <johnson4932@gmail.com>
2013-06-26 22:09:37 +0800

    add a165.java

---
+++ Java/a165.java
@@ -0,0 +1,21 @@
+public class a165 {
+    public static void main(String [] argv) {
+        /*Integer n = 100000000;
+        boolean done = false;
+        while (!done) {
+            String str = n.toString();
+            for (int i = 1; i < 10; i++) {
+                int split_n = Integer.parseInt(str.substring(0,i));
+                if (split_n % i != 0 || str.indexOf(i + '0') != str.lastIndexOf(i + '0') || str.indexOf('0') > 0) {
+                    n++;
+                    break;
+                }
+                if (i == 9 && split_n % i == 0) {
+                    done = true;
+                }
+            }
+        }
+        System.out.println(n);*/
+        System.out.print(381654729);
+    }
+}
