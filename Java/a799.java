0b5b6edcb0e28d84cfe0bcea560fb2c9f646fb1c
JohnsonLu <johnson4932@gmail.com>
2013-11-18 13:36:57 +0000

    add a799.java

---
+++ Java/a799.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class a799 {
+    public static void main(String [] argv) {
+        Scanner cin = new Scanner(System.in);
+        while (cin.hasNext()) {
+            int num = cin.nextInt();
+            System.out.println(Math.abs(num));
+        }
+    }
+}
